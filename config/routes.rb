28e6e2e772a0a74991979949a7b9d2f96f15034c
Wasi Ur Rahman <wasi.rahman@welldev.io>
2025-09-22 18:18:43 +0600

    Configuring Installed gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users
+  root "home#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -11,4 +13,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
 end
