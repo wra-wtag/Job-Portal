6b704fe8bed0bf786ff0936f9bbedff9700dbb25
Wasi Ur Rahman <wasi.rahman@welldev.io>
2025-09-24 14:16:59 +0600

    Updated Home and User controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+
+    def show
+        authorize @user
+    end
+
+    def edit
+        authorize @user
+    end
+
+    def update
+        authorize @user
+        if @user.update(user_params)
+            redirect_to profile_path, notice: "Profile Added Successfully"
+        else
+            render :edit
+        end
+    end
+
+    def setup
+        @user = current_user
+        authorize @user
+    end
+
+    private
+
+    def set_user
+        @user = params[:id] ? User.find(params[:id]) : current_user
+    end
+
+    def user_params
+        params.require(:user).permit(
+            :first_name, :last_name, :username, :bio, :location, :resume, notification_preferences: {}
+        )
+    end
+end
