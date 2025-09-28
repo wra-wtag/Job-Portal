a5800b5eb4f47528301cb252b746f9312e743da9
Wasi Ur Rahman <wasi.rahman@welldev.io>
2025-09-28 10:35:47 +0600

    Adding and configuring letter_opener gem

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   # root "posts#index"
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
