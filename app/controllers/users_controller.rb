3ace757545f15df8562510d31a4c1cac4c9a2383
Wasi Ur Rahman <wasi.rahman@welldev.io>
2025-09-28 14:30:37 +0600

    Updated Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :authorize_job_seeker!, only: [:edit, :update, :setup]
 
     def show
         authorize @user
@@ -31,7 +32,14 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(
-            :first_name, :last_name, :username, :bio, :location, :resume, notification_preferences: {}
+            :first_name, :last_name, :username, :bio, :location, :resume, skills_list: [], notification_preferences: {}
         )
+    end
+
+    def authorize_job_seeker!
+        return if current_user.job_seeker?
+
+        flash[:alert] = "Access denied: Only job seekers can edit profiles."
+        redirect_to root_path
     end
 end
