e5d00574679bbb2e2bf374319bc334aa1e4e70b7
Wasi Ur Rahman <wasi.rahman@welldev.io>
2025-09-30 11:30:50 +0600

    added double quote and end of line

--- app/policies/job_application_policy.rb
+++ app/policies/job_application_policy.rb
@@ -12,7 +12,7 @@ class JobApplicationPolicy < ApplicationPolicy
     def create?
         user.present? && user.job_seeker?
     end
-    
+
     def update?
         user.present? && (
             user.admin? || (user.recruiter? && record.company.recruiters.include?(user))
