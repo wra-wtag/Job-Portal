fc6e0b39eccd52b974392bb59ca66d220e3d29bb
Wasi Ur Rahman <wasi.rahman@welldev.io>
2025-10-01 15:22:48 +0600

    Updated Notification model enum

--- db/migrate/20251001075452_change_notification_kind_to_enum.rb
+++ db/migrate/20251001075452_change_notification_kind_to_enum.rb
@@ -12,9 +12,16 @@ class ChangeNotificationKindToEnum < ActiveRecord::Migration[8.0]
       when 'job_recommendation'
         notification.update_column(:kind_temp, 2)
       when 'system_announcement'
-        notification.update_column(:kind_temp, 2)
+        notification.update_column(:kind_temp, 3)
+      when 'recruiter_approved'
+        notification.update_column(:kind_temp, 4)
+      when 'recruiter_rejected'
+        notification.update_column(:kind_temp, 5)
+      when 'admin_approved_recruiter'
+        notification.update_column(:kind_temp, 6)
       end
     end
+
     remove_column :notifications, :kind
     rename_column :notifications, :kind_temp, :kind
 
@@ -35,8 +42,15 @@ class ChangeNotificationKindToEnum < ActiveRecord::Migration[8.0]
         notification.update_column(:kind_temp, 'job_recommendation')
       when 3
         notification.update_column(:kind_temp, 'system_announcement')
+      when 4
+        notification.update_column(:kind_temp, 'recruiter_approved')
+      when 5
+        notification.update_column(:kind_temp, 'recruiter_rejected')
+      when 6
+        notification.update_column(:kind_temp, 'admin_approved_recruiter')
       end
     end
+
     remove_column :notifications, :kind
     rename_column :notifications, :kind_temp, :kind
 
